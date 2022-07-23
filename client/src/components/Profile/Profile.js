a039c1769e6bdb45fad5943cac60ddf3d912da6e
aashutosh402 <chandrawanshiaashutosh@gmail.com>
2022-07-23 10:23:01 +0530

    changed

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,9 +1,17 @@
 import axios from 'axios'
-import React from 'react'
+import React,{useState} from 'react'
 import { ProfileNav } from './ProfileNav'
+import { useSelector, useDispatch } from "react-redux";
 
 export const Profile = () => {
+  const { user, isAuthenticated, loading,error } = useSelector((state) => state.user);
+
   const data= axios.post(`/api/auth/dashboard`)
+
+  const [name,setName] = useState("")
+  const [date,setDate]  = useState(``)
+  const [gender,setGender]  = useState("")
+
 
   return (<>
       <div style={{display:"flex"}}>
@@ -13,13 +21,13 @@ export const Profile = () => {
 
           <div className="pic_flex_box">
             <img  src={process.env.PUBLIC_URL + "img/1.png"}alt="rec" />
-            <button className="big_btn">Upload Photo</button>
+            <input className="big_btn" value ="Upload Photo"/>
           </div>
           <h2 className="per_text">Personal Details</h2>
-          <input className="name" type="text" placeholder="Full name" />
+          <input onChange= {(e)=>{setName(e.target.value)}} value = {user.username}  className="name" type="text" placeholder="Full name" />
           <div className="input_flex_box">
-            <input className="dob" type="date" placeholder="Date of Birth" />
-            <input className="gender" type="text" placeholder="Gender(Optinal)" />
+            <input onChange = {(e)=>{setDate(e.target.value)}} value={user.dob}  className="dob"  placeholder="Date of Birth" />
+            <input  onChange = {(e)=>{setGender(e.target.value)}} value = {user.gender} className="gender" type="text" placeholder="Gender(Optinal)" />
           </div>
           <div className="button_flex_box">
             <button className="dis_btn">Discard</button>
