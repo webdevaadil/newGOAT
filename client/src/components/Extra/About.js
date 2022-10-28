f119f52c94e4c76b0297dfaa49a7e33b1ea3101e
Anurag Pandey <anurag.pandey17@gmail.com>
2022-10-28 13:08:49 +0530

    about

--- client/src/components/Extra/About.js
+++ client/src/components/Extra/About.js
@@ -4,75 +4,173 @@ import { Header } from "../Header/Header";
 import "./impo.css";
 export const Aboutus = () => {
   useEffect(() => {
-    window.scrollTo(150, 0)
-  }, [])
-  return (<>
-      <Header/>
+    window.scrollTo(150, 0);
+  }, []);
+  return (
+    <>
+      <Header />
 
-    <div
-      style={{
-        margin: "0 auto",
-        width: "80%",
-        display: "flex",
-        justifyContent: "center",
-        flexDirection: "column",
-        alignItems: "center",
-        padding:"100px"
-        
-      }}
-    >
-      {" "}
-      <div className="box-about">
-        <h2 className="pro_heading">Terms of Service</h2>
-        <p className="text_content">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Blandit
-          tincidunt pellentesque egestas eu rutrum sed. Consectetur urna tempus
-          rhoncus volutpat aliquam vel ut nisi. Quisque cursus malesuada morbi
-          etiam condimentum quis. Ac eleifend molestie in sapien. Condimentum
-          leo amet, dolor ultrices quam in. Et, nunc orci, in mattis quam.
-          Lectus sed consectetur nisl dolor pharetra sit morbi ut elementum. At
-          eu, amet praesent amet sed velit fringilla ipsum sit. Pharetra aliquet
-          hendrerit nulla eu. Pulvinar dolor blandit nibh semper commodo a at.
-          Nisl id vel nec sapien. Ipsum vel pulvinar non etiam nec. At nibh sit
-          quisque in scelerisque consectetur purus. Facilisis adipiscing
-          vulputate velit vitae, mus vitae. Sit a tortor sed et. Ultricies
-          integer aliquam et amet amet pharetra ornare lobortis sed. Nisi quis
-          sed tellus nisi et mi dictumst tristique sit. Leo, pharetra nisl orci,
-          nisl. Arcu, nisl et nulla et viverra sit sed erat. Lorem imperdiet
-          tincidunt nisi nulla amet, praesent sodales luctus ac. Ultrices
-          suscipit accumsan sit pretium fermentum lectus. Ullamcorper fringilla
-          urna ultrices sem scelerisque lectus leo. Gravida diam sagittis nisl
-          in vel sit bibendum. Tincidunt dictumst dui luctus dapibus convallis
-          elementum massa cursus. Nunc netus commodo orci enim commodo. Aliquet
-          sit dolor cum magna morbi auctor vel hac nisi.
-        </p>
+      <div
+        style={{
+          margin: "0 auto",
+          width: "80%",
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+          alignItems: "center",
+          padding: "100px",
+        }}
+      >
+        {" "}
+        <div className="box-about">
+          <h2 className="pro_heading">Terms and Conditions</h2>
+            <ul >
+              www.thegoatstips.com.au" www.thegoatstips.com.au, hereon referred
+              to as the “Website”, is a website owned and operated by The GOATs
+              Sport Tips Pty Ltd (Australian Company Number 649699516), hereon
+              referred to as “The GOATs Tips”.
+            </ul>
+            <ul>
+              The GOATs Tips is an organisation operating for purposes of
+              providing sports and racing industry content to sports and racing
+              enthusiasts.
+            </ul>
+            <ul>
+              By using this Website, purchasing its Products and Services, and
+              viewing its content, you confirm that you have read and understand
+              all of the statements, terms, and conditions listed on this page
+              (hereon referred to as the “Terms and Conditions”) and that you
+              agree with them.
+            </ul>
+            <ul>
+              Nothing in these Terms and Conditions limits or excludes any
+              guarantees, warranties, representations or conditions implied or
+              imposed by law, including the Australian Consumer Law (or any
+              liability under them) which by law may not be limited or excluded.
+            </ul>
+          <h2 className="pro_heading1">1) General:</h2>
+            <ul className="pro_heading1ul">
+                 <li>
+
+        a) You are required to be at least 18 years old to access this Website or purchase The GOATs Tips’ Products and Services.
+                 </li>
+                 <li>
+
+        b) The GOATs Tips does not provide financial advice.
+                 </li>
+                 <li>
+                  
+        c) The GOATs Tips does not provide gambling bookmaking services. 
+                 </li>
+                 <li>
+
+        d) The GOATs Tips does not collect, process, transact, recommend, or pay out bets of any kind.
+                 </li>
+                 <li>
+
+        e) The GOATs Tips broadcasts both verified and unverified sports and racing content, sports and racing news, and subscription services to its users via the Website, e-mail and various other messaging mediums.
+                 </li>
+                 <li>
+
+        f) The GOATs Tips may, at any time, suspend your access to its content and supply of its Products and Services if it has any reason to believe that you have breached its Terms and Conditions.
+                 </li>
+                 <li>
+                  
+        g) Gambling is purely at a person’s own risk. The GOATs Tips is not responsible for members’ gambling choices and will not provide refunds in respect of gambling monies lost.
+                 </li>
+                 <li>
+
+        h) The GOATs Tips does not provide any assurances or guarantees that any of the sports and racing events which have been the subject of broadcasts or statements by The GOATs Tips have not been cancelled, postponed or otherwise varied. In the event that any such an event or race is cancelled, postponed or varied, The GOATs Tips accepts no responsibility.
+                 </li>
+                 <li>
+
+        i) Sports and racing results may be recorded and tracked at the prices available for races or sports events with bookmakers at the time tips are issued to you. We do not guarantee you will be able to achieve these prices as the betting agency or bookmaker service is not operated by The GOATs Tips.
+                 </li>
+                 <li>
+
+        j) To comply with relevant government requirements, you may be required to provide personal information about yourself (such as
+                 </li>
+            </ul>
+          <h2 className="pro_heading">Terms and Conditions</h2>
+            <ul>
+              www.thegoatstips.com.au" www.thegoatstips.com.au, hereon referred
+              to as the “Website”, is a website owned and operated by The GOATs
+              Sport Tips Pty Ltd (Australian Company Number 649699516), hereon
+              referred to as “The GOATs Tips”.
+            </ul>
+            <ul>
+              The GOATs Tips is an organisation operating for purposes of
+              providing sports and racing industry content to sports and racing
+              enthusiasts.
+            </ul>
+            <ul>
+              By using this Website, purchasing its Products and Services, and
+              viewing its content, you confirm that you have read and understand
+              all of the statements, terms, and conditions listed on this page
+              (hereon referred to as the “Terms and Conditions”) and that you
+              agree with them.
+            </ul>
+            <ul>
+              Nothing in these Terms and Conditions limits or excludes any
+              guarantees, warranties, representations or conditions implied or
+              imposed by law, including the Australian Consumer Law (or any
+              liability under them) which by law may not be limited or excluded.
+            </ul>
+          <h2 className="pro_heading">Terms and Conditions</h2>
+            <ul>
+              www.thegoatstips.com.au" www.thegoatstips.com.au, hereon referred
+              to as the “Website”, is a website owned and operated by The GOATs
+              Sport Tips Pty Ltd (Australian Company Number 649699516), hereon
+              referred to as “The GOATs Tips”.
+            </ul>
+            <ul>
+              The GOATs Tips is an organisation operating for purposes of
+              providing sports and racing industry content to sports and racing
+              enthusiasts.
+            </ul>
+            <ul>
+              By using this Website, purchasing its Products and Services, and
+              viewing its content, you confirm that you have read and understand
+              all of the statements, terms, and conditions listed on this page
+              (hereon referred to as the “Terms and Conditions”) and that you
+              agree with them.
+            </ul>
+            <ul>
+              Nothing in these Terms and Conditions limits or excludes any
+              guarantees, warranties, representations or conditions implied or
+              imposed by law, including the Australian Consumer Law (or any
+              liability under them) which by law may not be limited or excluded.
+            </ul>
+          
+        </div>
+
+        {/* <div className="box-about">
+          <h2 className="pro_heading">Privacy Policy</h2>
+          <p className="text_content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Blandit
+            tincidunt pellentesque egestas eu rutrum sed. Consectetur urna
+            tempus rhoncus volutpat aliquam vel ut nisi. Quisque cursus
+            malesuada morbi etiam condimentum quis. Ac eleifend molestie in
+            sapien. Condimentum leo amet, dolor ultrices quam in. Et, nunc orci,
+            in mattis quam. Lectus sed consectetur nisl dolor pharetra sit morbi
+            ut elementum. At eu, amet praesent amet sed velit fringilla ipsum
+            sit. Pharetra aliquet hendrerit nulla eu. Pulvinar dolor blandit
+            nibh semper commodo a at. Nisl id vel nec sapien. Ipsum vel pulvinar
+            non etiam nec. At nibh sit quisque in scelerisque consectetur purus.
+            Cursus nec gravida sem nisl. Lorem vel lorem nisl mauris. Arcu duis
+            libero massa volutpat feugiat augue sapien, dignissim vivamus.
+            Faucibus tristique sed posuere felis tellus scelerisque iaculis
+            pellentesque. Amet dignissim at duis id et viverra elementum. Urna
+            sed enim quis leo amet. Quis sit leo congue odio. Risus, ut in
+            tempor in consequat ut. Consequat non ac eu porta volutpat tempor.
+            Faucibus integer hendrerit etiam egestas at. Purus nisl in varius
+            volutpat lorem tortor bibendum non sit. Pellentesque tempor tellus
+            nunc, ultricies gravida pulvinar. Eget dictum suscipit leo, amet.
+            Quis venenatis vel id tristique vitae rhoncus ultrices.
+          </p>
+        </div> */}
       </div>
-      <div className="box-about">
-        <h2 className="pro_heading">Privacy Policy</h2>
-        <p className="text_content">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Blandit
-          tincidunt pellentesque egestas eu rutrum sed. Consectetur urna tempus
-          rhoncus volutpat aliquam vel ut nisi. Quisque cursus malesuada morbi
-          etiam condimentum quis. Ac eleifend molestie in sapien. Condimentum
-          leo amet, dolor ultrices quam in. Et, nunc orci, in mattis quam.
-          Lectus sed consectetur nisl dolor pharetra sit morbi ut elementum. At
-          eu, amet praesent amet sed velit fringilla ipsum sit. Pharetra aliquet
-          hendrerit nulla eu. Pulvinar dolor blandit nibh semper commodo a at.
-          Nisl id vel nec sapien. Ipsum vel pulvinar non etiam nec. At nibh sit
-          quisque in scelerisque consectetur purus. Cursus nec gravida sem nisl.
-          Lorem vel lorem nisl mauris. Arcu duis libero massa volutpat feugiat
-          augue sapien, dignissim vivamus. Faucibus tristique sed posuere felis
-          tellus scelerisque iaculis pellentesque. Amet dignissim at duis id et
-          viverra elementum. Urna sed enim quis leo amet. Quis sit leo congue
-          odio. Risus, ut in tempor in consequat ut. Consequat non ac eu porta
-          volutpat tempor. Faucibus integer hendrerit etiam egestas at. Purus
-          nisl in varius volutpat lorem tortor bibendum non sit. Pellentesque
-          tempor tellus nunc, ultricies gravida pulvinar. Eget dictum suscipit
-          leo, amet. Quis venenatis vel id tristique vitae rhoncus ultrices.
-        </p>
-      </div>
-    </div>
-      <Footer/>
-        </>
+      <Footer />
+    </>
   );
 };
