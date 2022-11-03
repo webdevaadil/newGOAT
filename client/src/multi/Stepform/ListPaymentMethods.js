5c52877f58f852433120ecbf88e045eb897037ca
Anurag Pandey <anurag.pandey17@gmail.com>
2022-11-03 11:40:10 +0530

    stripe 7 build

--- client/src/multi/Stepform/ListPaymentMethods.js
+++ client/src/multi/Stepform/ListPaymentMethods.js
@@ -3,48 +3,10 @@ import style from "./ListPaymentMethods.css";
 
 import { format } from "date-fns";
 import axios from "axios";
-import visa from "../assets/cards/visa.png";
-import americanexpress from "../assets/cards/americanexpress.png";
-import dinersclub from "../assets/cards/dinersclub.jpg";
-import discover from "../assets/cards/discover.jpg";
-import elo from "../assets/cards/elo.png";
-import hiper from "../assets/cards/hiper.png";
-import jcb from "../assets/cards/jcb.png";
-import mastercard from "../assets/cards/mastercard.png";
-import mir from "../assets/cards/mir.png";
-import unionpay from "../assets/cards/unionpay.png";
 import { useSelector } from "react-redux";
 
-function getCardImage(type) {
-  switch (type) {
-    case "visa":
-      return visa;
-    case "mastercard":
-      return mastercard;
-    case "amex":
-      return americanexpress;
-    case "diners club":
-      return dinersclub;
-    case "discover":
-      return discover;
-    case "jcb":
-      return jcb;
-    case "unionpay":
-      return unionpay;
-    case "maestro":
-      return mastercard;
-    case "mir":
-      return mir;
-    case "elo":
-      return elo;
-    case "hiper":
-      return hiper;
-    case "hipercard":
-      return hiper;
-    default:
-      return visa;
-  }
-}
+
+
 export default function ListPaymentMethods({ handleSelectCard }) {
   const [paymentMethods, setPaymentMethods] = useState(null);
   const { error, loading, isAuthenticated, user } = useSelector(
